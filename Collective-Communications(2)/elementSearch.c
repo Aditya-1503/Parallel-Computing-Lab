6140e9ef462aece8af9d72514414a520ef693440
Aditya-1503 <adityaagg15@gmail.com>
2025-02-07 23:03:42 +0530

    updating error-handling

--- Collective-Communications(2)/elementSearch.c
+++ Collective-Communications(2)/elementSearch.c
@@ -2,8 +2,20 @@
 #include<stdlib.h>
 #include "mpi.h"
 
+
+void handle_mpi_error(int errcode) {
+    char error_string[MPI_MAX_ERROR_STRING];
+    int length_of_error_string;
+    MPI_Error_string(errcode, error_string, &length_of_error_string);
+    fprintf(stderr, "MPI Error: %s\n", error_string);
+    MPI_Abort(MPI_COMM_WORLD, errcode);
+}
+
+
 int main(int argc, char* argv[]){
     int rank, size, arr[3][3], toSearch, b[3], ans=0, out;
+    int errcode;
+
     MPI_Init( &argc, &argv);
     MPI_Comm_rank( MPI_COMM_WORLD , &rank);
     MPI_Comm_size( MPI_COMM_WORLD , &size);
@@ -18,7 +30,10 @@ int main(int argc, char* argv[]){
         scanf("%d", &toSearch);
     }
     MPI_Bcast( &toSearch , 1 , MPI_INT , 0 , MPI_COMM_WORLD);
-    MPI_Scatter( arr , 3 , MPI_INT , b , 3 , MPI_INT , 0 , MPI_COMM_WORLD);
+    errcode = MPI_Scatter( arr , 3 , MPI_INT , b , 3 , MPI_INT , 0 , MPI_COMM_WORLD);
+    if (errcode != MPI_SUCCESS){
+        handle_mpi_error(errcode);
+    }
     for(int i=0; i<3;i++){
         if (b[i] == toSearch)
             ans += 1;
